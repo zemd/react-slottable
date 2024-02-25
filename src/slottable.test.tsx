0640223865be0adcbd6451cfa0bb033d6d1810a9
Dmytro Zelenetskyi <dmitry.zelenetskiy@gmail.com>
2024-02-25 21:46:56 +0100

    allowing undefined and null values as valid className prop

--- src/slottable.test.tsx
+++ src/slottable.test.tsx
@@ -1,6 +1,6 @@
 import { slottable } from "./slottable";
 import { render } from "@testing-library/react";
-import type { TSlottablePropsFactory } from "dist";
+import type { TSlottablePropsFactory } from "./types";
 import { describe, test, expect } from "vitest";
 
 describe("slottable", () => {
@@ -8,12 +8,12 @@ describe("slottable", () => {
     const Component = slottable<"div", TSlottablePropsFactory<object>>(
       ({ children }) => {
         return <div>{children}</div>;
-      },
+      }
     );
     const { getByText } = render(
       <Component>
         <div data-testid="test-child">Test child</div>
-      </Component>,
+      </Component>
     );
     expect(typeof Component).toBe("object"); // slottable returns Exotic component
     expect(getByText("Test child")).toBeInTheDocument();
@@ -24,8 +24,38 @@ describe("slottable", () => {
       ({ children }) => {
         return <div>{children}</div>;
       },
-      "TestComponent",
+      "TestComponent"
     );
     expect(Component.displayName).toBe("TestComponent");
+  });
+
+  test("should accept undefined as a valid value for className prop", () => {
+    const Component = slottable<"div", TSlottablePropsFactory<object>>(
+      ({ children }) => {
+        return <div>{children}</div>;
+      }
+    );
+    const { getByText } = render(
+      <Component className={undefined}>
+        <div data-testid="test-child">Test child</div>
+      </Component>
+    );
+    expect(typeof Component).toBe("object"); // slottable returns Exotic component
+    expect(getByText("Test child")).toBeInTheDocument();
+  });
+
+  test("should accept null as a valid value for className prop", () => {
+    const Component = slottable<"div", TSlottablePropsFactory<object>>(
+      ({ children }) => {
+        return <div>{children}</div>;
+      }
+    );
+    const { getByText } = render(
+      <Component className={null}>
+        <div data-testid="test-child">Test child</div>
+      </Component>
+    );
+    expect(typeof Component).toBe("object"); // slottable returns Exotic component
+    expect(getByText("Test child")).toBeInTheDocument();
   });
 });
